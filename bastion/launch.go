31ae9ebc3445f1487bee0fbdd509f0877368c54d
Tarun Menon <tarunmenon95@gmail.com>
2022-09-13 14:58:37 +1000

    Refactoring

--- bastion/launch.go
+++ bastion/launch.go
@@ -14,99 +14,13 @@ import (
 )
 
 func CmdLaunchLinuxBastion(c *cli.Context) error {
-	var (
-		err               error
-		id                string
-		sess              *session.Session
-		ami               string
-		instanceProfile   string
-		sshKey            string
-		launchedBy        string
-		expire            bool
-		expireAfter       int
-		subnet            subnet
-		subnetId          string
-		securitygroupId   string
-		instanceType      string
-		keyName           string
-		userdata          string
-		spot              bool
-		bastionInstanceId string
-	)
 
-	id = GenerateSessionId()
-	log.Println("bastion session id: " + id)
-
-	sess = SetupAWSSession(c.String("region"), c.String("profile"))
-
-	ami, err = GetAndValidateAmi(sess, c.String("ami"), c.String("instance-type"))
+	bastionInstanceId, _, err := CreateBastion(c)
 	if err != nil {
 		return err
 	}
 
-	instanceType = c.String("instance-type")
-
-	instanceProfile, err = GetIAMInstanceProfile(sess)
-	if err != nil {
-		return err
-	}
-
-	if c.String("ssh-key") != "" {
-		sshKey, err = ReadAndValidatePublicKey(c.String("ssh-key"))
-		if err != nil {
-			return err
-		}
-	}
-
-	launchedBy, err = LookupUserIdentity(sess)
-	if err != nil {
-		return err
-	}
-
-	expireAfter = c.Int("expire-after")
-	expire = true
-	if c.Bool("no-expire") {
-		expire = false
-	}
-
-	spot = true
-	if c.Bool("no-spot") {
-		spot = false
-	}
-
-	subnetId = c.String("subnet-id")
-	if subnetId == "" {
-		subnets, err := GetSubnets(sess)
-		if err != nil {
-			return err
-		}
-
-		subnet = SelectSubnet(subnets)
-		subnetId = subnet.SubnetId
-	} else {
-		subnet, err = GetSubnet(sess, subnetId)
-		if err != nil {
-			return err
-		}
-	}
-
-	securitygroupId = c.String("security-group-id")
-	if securitygroupId == "" {
-		securitygroups, err := GetSecurityGroups(sess, subnet.VpcId)
-		if err != nil {
-			return err
-		}
-
-		securitygroup := SelectSecurityGroup(securitygroups)
-		securitygroupId = securitygroup.SecurityGrouId
-	}
-
-	userdata = BuildLinuxUserdata(sshKey, c.String("ssh-user"), expire, expireAfter, c.String("efs"), c.String("access-points"))
-
-	bastionInstanceId, err = StartEc2(id, sess, ami, instanceProfile, subnetId, securitygroupId, instanceType, launchedBy, userdata, keyName, spot)
-	if err != nil {
-		return err
-	}
+	sess := SetupAWSSession(c.String("region"), c.String("profile"))
 
 	if c.Bool("ssh") {
 		// need to wait EC2 status ok to wait for userdata to complete
@@ -139,6 +53,105 @@ func CmdLaunchLinuxBastion(c *cli.Context) error {
 	}
 
 	return nil
+}
+
+func CreateBastion(c *cli.Context) (string, string, error) {
+	///Function to create a bastion instance with 'default' parameters
+	var (
+		err               error
+		id                string
+		sess              *session.Session
+		ami               string
+		instanceProfile   string
+		sshKey            string
+		launchedBy        string
+		expire            bool
+		expireAfter       int
+		subnet            subnet
+		subnetId          string
+		securitygroupId   string
+		instanceType      string
+		keyName           string
+		userdata          string
+		spot              bool
+		bastionInstanceId string
+	)
+	//Check if theres a better way to create a default instance? eg: call CmdLaunchLinuxBastion with some spoofed cli context? but somehow return instance id
+
+	id = GenerateSessionId()
+	log.Println("bastion session id: " + id)
+
+	sess = SetupAWSSession(c.String("region"), c.String("profile"))
+
+	ami, err = GetAndValidateAmi(sess, c.String("ami"), c.String("instance-type"))
+	if err != nil {
+		return "", "", err
+	}
+
+	instanceType = c.String("instance-type")
+
+	instanceProfile, err = GetIAMInstanceProfile(sess)
+	if err != nil {
+		return "", "", err
+	}
+
+	if c.String("ssh-key") != "" {
+		sshKey, err = ReadAndValidatePublicKey(c.String("ssh-key"))
+		if err != nil {
+			return "", "", err
+		}
+	}
+
+	launchedBy, err = LookupUserIdentity(sess)
+	if err != nil {
+		return "", "", err
+	}
+
+	expireAfter = c.Int("expire-after")
+	expire = true
+	if c.Bool("no-expire") {
+		expire = false
+	}
+
+	spot = true
+	if c.Bool("no-spot") {
+		spot = false
+	}
+
+	subnetId = c.String("subnet-id")
+	if subnetId == "" {
+		subnets, err := GetSubnets(sess)
+		if err != nil {
+			return "", "", err
+		}
+
+		subnet = SelectSubnet(subnets)
+		subnetId = subnet.SubnetId
+	} else {
+		subnet, err = GetSubnet(sess, subnetId)
+		if err != nil {
+			return "", "", err
+		}
+	}
+
+	securitygroupId = c.String("security-group-id")
+	if securitygroupId == "" {
+		securitygroups, err := GetSecurityGroups(sess, subnet.VpcId)
+		if err != nil {
+			return "", "", err
+		}
+
+		securitygroup := SelectSecurityGroup(securitygroups)
+		securitygroupId = securitygroup.SecurityGrouId
+	}
+
+	userdata = BuildLinuxUserdata(sshKey, c.String("ssh-user"), expire, expireAfter, c.String("efs"), c.String("access-points"))
+
+	bastionInstanceId, err = StartEc2(id, sess, ami, instanceProfile, subnetId, securitygroupId, instanceType, launchedBy, userdata, keyName, spot)
+	if err != nil {
+		return "", "", err
+	}
+	return bastionInstanceId, securitygroupId, err
 }
 
 func CmdLaunchWindowsBastion(c *cli.Context) error {
